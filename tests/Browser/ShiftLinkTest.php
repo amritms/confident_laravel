6de92089106f5e17a04b7d52bcf34c3bdfac9566
Amrit Shrestha <amritms@gmail.com>
2020-12-19 16:51:41 -0500

    confident laravel setup

---
+++ tests/Browser/ShiftLinkTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Browser;
+
+use Tests\DuskTestCase;
+use Laravel\Dusk\Browser;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class ShiftLinkTest extends DuskTestCase
+{
+    /**
+     * @test
+     */
+    public function it_links_to_laravel_shift_page(): void
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/')
+                    ->assertSee('Laravel Shift')
+            ->clickLink('Laravel Shift')
+            ->assertUrlIs('https://laravelshift.com/');
+        });
+    }
+}
